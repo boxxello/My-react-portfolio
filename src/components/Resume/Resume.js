89daf08cb0888791c3e70df4ae83f3238cfb64b0
boxxoA <francihula12@gmail.com>
2022-09-01 16:40:30 +0200

    Full commit of my portfolio.

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -34,11 +34,6 @@ function Resume() {
 
                     </Document>
                 </Row>
-                <Row className="resume">
-                    <Document file={resumeLink} className="d-flex justify-content-center">
-                        <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6}/>
-                    </Document>
-                </Row>
 
 
                 <Row style={{justifyContent: "center", position: "relative"}}>
